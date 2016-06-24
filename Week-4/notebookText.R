3e2478f036a7498dd414cfc3f888705996e494e7
Matthew Moocarme <moocarme@gmail.com>
2016-06-23 21:14:12 -0400

    Add files via upload

---
+++ Week-4/notebookText.R
@@ -0,0 +1,13 @@
+#' ---
+#' title: "R Notebook example"
+#' author: "Matt Moocarme"
+#' date: "`r format(Sys.time(), '%d %B, %Y')`"
+#' output: html_document
+#' ---
+#   
+
+#+ knitr::opts_chunk$set(echo = TRUE, eval = FALSE, comment = '')
+
+
+#+ eval = T
+head(mtcars)
